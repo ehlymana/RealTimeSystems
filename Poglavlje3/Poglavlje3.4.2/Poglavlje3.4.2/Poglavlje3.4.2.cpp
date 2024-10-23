edbe0e9d20b349ade4ac03fc57e15c2257b839f3
Ehlimana <ekrupalija1@etf.unsa.ba>
2024-10-23 09:12:54 +0200

    ostatak commita

---
+++ Poglavlje3/Poglavlje3.4.2/Poglavlje3.4.2/Poglavlje3.4.2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <chrono>
+#include <thread>
+#include <windows.h>
+#include <processthreadsapi.h>
+
+int maxSize = 10;
+std::vector<std::string> students;
+
+bool FindAutomatically(std::string name)
+{
+	return (std::find(students.begin(), students.end(),
+		name) != students.end());
+}
+
+void Measurement(std::string indexToFind, int threadNo)
+{
+	std::cout << "THREAD NO. " << threadNo << " MEASUREMENT BEGINS" << std::endl;
+
+	int noOfRepetitions = 1000;
+	for (int i = 0; i < noOfRepetitions; i++)
+	{
+		auto start = std::chrono::high_resolution_clock::now();
+		FindAutomatically("Name " + indexToFind);
+		auto stop = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+		std::cout << "Thread no. " << threadNo << ", Iteration " << i + 1 << ": " << duration << std::endl;
+	}
+	std::cout << "THREAD NO. " << threadNo << " MEASUREMENT ENDS" << std::endl;
+}
+
+void TimerTrigger(std::string indexToFind, int threadNo)
+{
+	std::thread thread(Measurement, indexToFind, threadNo);
+	thread.detach();
+}
+
+int main()
+{
+	maxSize = 1000000;
+	for (int i = 0; i < maxSize; i++)
+		students.push_back("Name " + std::to_string(i));
+
+	std::string indexToFind = std::to_string(maxSize - 1);
+
+	double delay = 30000;
+	delay *= CLOCKS_PER_SEC / 1000;
+	int i = 0;
+	while (1 == 1)
+	{
+		TimerTrigger(indexToFind, i);
+		i++;
+
+		clock_t now = clock();
+		while (clock() - now < delay);
+	}
+}
